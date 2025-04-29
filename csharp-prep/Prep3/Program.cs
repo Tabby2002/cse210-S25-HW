75068bed15e2d2dc0295978c1a7896b5d635779e
Tabitha Harrington <tabithaharrington@MacBookAir.lan>
2025-04-28 19:13:40 -0600

    Guessing number game created

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    { 
+        //Console.Write("What is the magic number? ");
+        //string valueFromUser = Console.ReadLine();
+        //int x = int.Parse(valueFromUser);
+        //int y = -1;
+        
+        Random randomGenerator = new Random();
+        int x = randomGenerator.Next(1, 100);
+        int y = -1;
+
+        while (x != y)
+        {
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        y = int.Parse(guess);
+        if (x == y)
+        {
+            Console.Write("You guessed it.");
+        }
+        else if (x < y)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("Higher");
+        }
+        
+        }       
     }
 }
