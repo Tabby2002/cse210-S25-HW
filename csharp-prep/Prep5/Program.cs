fb84da1e6714c75604594da85d4743985c188504
Tabitha Harrington <tabithaharrington@MacBookAir.lan>
2025-05-01 15:56:45 -0600

    Functions prep 5 assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Data.SqlTypes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the program!");
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        Console.Write("Please enter your favorite number: ");
+        string number = Console.ReadLine();
+        int n = int.Parse(number);
+        int sq = n * n;
+        Console.Write($"{name}, the square of your number is {sq}.");
     }
 }
