df09eed71679167cd1d89c5b5ca0f651f6cdcbb5
Tabitha Harrington <tabithaharrington@MacBookAir.lan>
2025-05-07 23:32:03 -0600

    updated main program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,5 +1,4 @@
 using System;
-//using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
@@ -18,7 +17,7 @@ class Program
         job2._endYear = 2023;
 
         Resume myResume = new Resume();
-        myResume._name = "John Doe";
+        myResume._name = "Allison Rose";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
