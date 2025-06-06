2cb593540c2cdc3b38c5130bc63695c84cd337ef
Tabitha Harrington <tabithaharrington@MacBookAir.lan>
2025-06-06 16:22:25 -0600

    Learning 4 Homework Assignment

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,26 @@
+public class MathAssignment : Assignment
+{
+    private string _textbookSection = "";
+    private string _problems = "";
+
+    public string GetTextbookSection()
+    {
+        return _textbookSection;
+    }
+    public void SetTextbookSection(string section)
+    {
+        _textbookSection = section;
+    }
+    public string GetProblems()
+    {
+        return _problems;
+    }
+    public void SetProblems(string problem)
+    {
+        _problems = problem;
+    }
+    public string GetHomeworkList()
+    {
+        return $"{_textbookSection} {_problems}";
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assign1 = new Assignment();
+        assign1.SetStudentName("Samuel Bennett");
+        assign1.SetTopic("Multiplication");
+
+        Assignment assign2 = new Assignment();
+        assign2.SetStudentName("Roberto Rodriguez");
+        assign2.SetTopic("Fractions");
+        MathAssignment assign21 = new MathAssignment();
+        assign21.SetTextbookSection("Section 7.3");
+        assign21.SetProblems("Problems 8-9");
+
+        Assignment assign3 = new Assignment();
+        assign3.SetStudentName("Mary Waters");
+        assign3.SetTopic("European History");
+        WritingAssignment assign31 = new WritingAssignment();
+        assign31.SetTitle("The Causes of World War II by Mary Waters");
+
+
+        Console.WriteLine(assign1.GetSummary());
+        
+        Console.WriteLine(assign2.GetSummary());
+        Console.WriteLine(assign21.GetHomeworkList());
+        Console.WriteLine(assign3.GetSummary());
+        Console.WriteLine(assign31.GetWritingInformation());
     }
 }
